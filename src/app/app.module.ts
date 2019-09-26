e06e78adc2493baaa30dd20276c2164bffbb0336
thaiduongcao <thaiduongcao@users.noreply.github.com>
2019-09-26 10:38:53 +0700

    btl

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TrangchuComponent } from './trangchu/trangchu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TrangchuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
